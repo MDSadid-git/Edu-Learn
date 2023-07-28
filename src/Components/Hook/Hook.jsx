7ed1d8731b004b2f23a2c91d2a7d7627d447c36c
Sadid <sfhsadid@gmail.com>
2023-07-29 00:39:53 +0600

    Sadid web site titel add done

--- src/Components/Hook/Hook.jsx
+++ src/Components/Hook/Hook.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const useTitle = (title) => {
   useEffect(() => {
-    document.title = `${title} - Weero Edu`;
+    document.title = `${title} - Edu Learn`;
   }, [title]);
 };
 export default useTitle;
