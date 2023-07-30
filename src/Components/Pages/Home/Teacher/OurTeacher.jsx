2b9b4f9282fcd4633921ff9192f84f5f056f56d6
Sadid <sfhsadid@gmail.com>
2023-07-31 00:42:24 +0600

    Home2 Ourteacher area done

--- src/Components/Pages/Home/Teacher/OurTeacher.jsx
+++ src/Components/Pages/Home/Teacher/OurTeacher.jsx
@@ -12,7 +12,7 @@ const OurTeacher = () => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-brand">
+            <h1 className="sm:text-4xl text-2xl font-bold title-font mb-4 text-brand">
               Our Teachers
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
